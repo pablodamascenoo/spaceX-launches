a9284ac94a3c1c4421630ea56a3de2f5b71f51fc
pablodamascenoo <pablofacex@gmail.com>
2022-10-21 11:27:15 -0300

    feat: added HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage/index.js";
+
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/HomePage/index.tsx
@@ -0,0 +1,9 @@
+import Header from "../../components/Header/index.js";
+
+export default function HomePage() {
+  return (
+    <main>
+      <Header />
+    </main>
+  );
+}
