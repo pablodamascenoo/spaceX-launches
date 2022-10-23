98402806cb3387e7848e92d4756b5a5e55be4436
pablodamascenoo <pablofacex@gmail.com>
2022-10-23 15:41:52 -0300

    feat: created UpcomingPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage/index.js";
+import UpcomingPage from "./pages/UpcomingPage/index.js";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/upcoming" element={<UpcomingPage />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/pages/UpcomingPage/index.tsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Card from "../../components/Card/index.js";
+import Header from "../../components/Header/index.js";
+import api from "../../services/api.js";
+import { Launch } from "../../types/Launch.js";
+
+export default function UpcomingPage() {
+  const [launches, setLaunches] = useState<Launch[] | []>([]);
+  const [hasMore, setHasMore] = useState({ more: false, nextPage: 1 });
+
+  function fetchData() {
+    const promisse = api.get(`/upcoming?page=${hasMore.nextPage}`);
+
+    promisse.then((obj) => {
+      const { docs, hasNextPage, nextPage } = obj.data;
+      setLaunches([...launches, ...docs]);
+      setHasMore({ more: hasNextPage, nextPage });
+    });
+
+    promisse.catch((error) => {
+      console.log(error);
+    });
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <main id="scrollableMain">
+      <Header />
+      <InfiniteScroll
+        scrollableTarget="scrollableMain"
+        className="scroller"
+        hasMore={hasMore.more}
+        next={fetchData}
+        dataLength={launches.length}
+        loader={<h4>Loading...</h4>}
+        endMessage={<p className="text-center text-lg">You have seen it all</p>}
+      >
+        {launches?.map((item, index) => {
+          const { crew, name, success, date_unix, links } = item;
+          return (
+            <Card
+              key={index}
+              crew={crew}
+              name={name}
+              success={success}
+              date={date_unix}
+              image={links.patch.small}
+            />
+          );
+        })}
+      </InfiniteScroll>
+    </main>
+  );
+}
